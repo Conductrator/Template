03904631586af8985eaa4a1eb31a2ce14bbba5fa
Fehmi <fehmiisener@gmail.com>
2023-01-28 14:33:59 +0300

    build: initialize the project with maven

---
+++ src/test/java/com/conductrator/blueprint/BlueprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conductrator.blueprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
